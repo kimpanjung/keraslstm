dccbb352d646900daa712e8aee459e39c6663cf8
pjkim <skofield@naver.com>
2019-07-22 22:36:14 +0900

    GUI 프로토타입

---
+++ ezKeras.py
@@ -0,0 +1,230 @@
+import tensorflow as tf
+import os
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from keras import optimizers
+
+# from tf.keras.models import Sequential  # This does not work!
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Input, Dense, GRU, Embedding
+from tensorflow.python.keras.optimizers import RMSprop
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard, ReduceLROnPlateau
+
+from pandas.tseries.offsets import MonthEnd
+from pandas.tseries.offsets import Minute
+
+# Normalize
+from sklearn.preprocessing import MinMaxScaler
+
+from keras.layers import LSTM
+from keras.models import Sequential
+from keras.layers import Dense
+import keras.backend as K
+from keras.callbacks import EarlyStopping
+
+from keras.models import load_model
+
+
+##############################################################
+
+def main():
+    machine = Machine()
+    machine.run(epochs=1, units=1, batch_size=512)
+
+# Machine learning class
+class Machine():
+    #def __init__(self):
+        #self.data = data
+        #self.fname = fname
+        #self.fname = fname
+        #self.data = Dataset(fname='C:\\Users\PJ\PycharmProjects\ezKeras\inner_data_new_0705.csv') # Data 생성
+        #shape = self.data.X.shape[1:] # LSTM 입력계층 크기 정의
+        #shape = self.data.X_train.shape[0]
+
+    # 실행함수
+    def datasetLoad(self, fname):
+        #self.data = Dataset(fname='C:\\Users\PJ\PycharmProjects\ezKeras\inner_data_new_0705.csv') # Data 생성
+        self.data = Dataset(fname=fname)
+        #shape = self.data.X.shape[1:] # LSTM 입력계층 크기 정의
+        shape = self.data.X_train.shape[0]
+
+    def totalDatasetNumber(self):
+        total = self.data.totalDataNumber()
+        return total
+
+    def trainDatasetNumber(self):
+        total = self.data.trainDataNumber()
+        return total
+
+    def testDatasetNumber(self):
+        total = self.data.testDataNumber()
+        return total
+
+    def datasethead(self):
+        total = self.data.df_temp.head(50)
+        return total
+
+
+
+
+    def run(self, epochs, units, batch_size):
+        d = self.data
+        X_train, X_test, y_train, y_test = d.X_train, d.X_test, d.y_train, d.y_test
+        X_train_t = d.X_train_t
+        X_test_t = d.X_test_t
+
+        K.clear_session()
+        model = Sequential()  # Sequeatial Model  # 190709 : 20
+        model.add(LSTM(units, input_shape=(177, 1)))  # (timestep, feature)
+        model.add(Dense(3))  # output = 1
+        model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+        model.summary()
+
+        early_stop = EarlyStopping(monitor='loss', patience=1, verbose=1)
+
+        hist = model.fit(d.X_train_t, d.y_train, epochs=epochs,
+                         batch_size=batch_size, verbose=1)
+        # , callbacks=[early_stop]))
+        print("result : \n" + str(hist))
+
+        from keras.models import load_model
+        model.save('190711_200_unit256_batch128.h5')
+
+        target_names = ['inner_temperature', 'inner_humidity', 'inner_co2']
+
+        y_pred = model.predict(X_test_t)
+
+        # %matplotlib inline
+
+        fig, loss_ax = plt.subplots()
+
+        acc_ax = loss_ax.twinx()
+
+        loss_ax.plot(hist.history['loss'], 'y', label='train loss')
+        acc_ax.plot(hist.history['acc'], 'b', label='train acc')
+
+        loss_ax.set_xlabel('epoch')
+        loss_ax.set_ylabel('loss')
+        acc_ax.set_ylabel('accuray')
+
+        loss_ax.legend(loc='upper left')
+        acc_ax.legend(loc='lower left')
+
+        plt.show()
+
+        y_pred = model.predict(X_test_t)
+        plt.figure(figsize=(10, 5))
+        plt.ylabel(target_names[1])
+        plt.plot(y_test[:, 1])
+        plt.plot(y_pred[:, 1])
+        plt.show()
+
+
+        y_pred = model.predict(X_test_t)
+        plt.figure(figsize=(10, 5))
+        plt.ylabel(target_names[0])
+        plt.plot(y_test[:, 0])
+        plt.plot(y_pred[:, 0])
+        plt.show()
+
+
+        y_pred = model.predict(X_test_t)
+        plt.figure(figsize=(10, 5))
+
+        plt.ylabel(target_names[2])
+        plt.plot(y_test[:, 2])
+        plt.plot(y_pred[:, 2])
+        plt.show()
+
+
+        print("\n Acc : %.4f" % (model.evaluate(X_train_t, y_train)[1]))
+
+        print("\n Loss : %.4f" % (model.evaluate(X_train_t, y_train)[0]))
+
+
+class Dataset:
+   # def __init__(self, fname='C:\\Users\PJ\PycharmProjects\ezKeras\inner_data_new_0705.csv'):
+    def __init__(self, fname):
+        self.fname = fname
+
+        df_temp = load_data(fname=self.fname)
+
+        split_date = pd.Timestamp('20190625 08:49:26')  # 5000
+
+        train = df_temp.loc[:split_date, ['inner_temperature', 'inner_humidity', 'inner_co2']]
+        test = df_temp.loc[split_date:, ['inner_temperature', 'inner_humidity', 'inner_co2']]
+
+        # Scaler()
+        sc = MinMaxScaler()
+
+        train_sc = sc.fit_transform(train)
+        test_sc = sc.transform(test)
+        train_sc_df = pd.DataFrame(train_sc, columns=['inner_temperature', 'inner_humidity', 'inner_co2'],
+                                   index=train.index)
+        test_sc_df = pd.DataFrame(test_sc, columns=['inner_temperature', 'inner_humidity', 'inner_co2'],
+                                  index=test.index)
+
+        for s in range(1, 60):
+            train_sc_df['shift_{}_1'.format(s)] = train_sc_df['inner_temperature'].shift(s)
+            train_sc_df['shift_{}_2'.format(s)] = train_sc_df['inner_humidity'].shift(s)
+            train_sc_df['shift_{}_3'.format(s)] = train_sc_df['inner_co2'].shift(s)
+
+            test_sc_df['shift_{}_1'.format(s)] = test_sc_df['inner_temperature'].shift(s)
+            test_sc_df['shift_{}_2'.format(s)] = test_sc_df['inner_humidity'].shift(s)
+            test_sc_df['shift_{}_3'.format(s)] = test_sc_df['inner_co2'].shift(s)
+
+        target_names = ['inner_temperature', 'inner_humidity', 'inner_co2']
+
+        # d.X_train, d.X_test, d.y_train, d.y_test
+        X_train = train_sc_df.dropna().drop(target_names, axis=1)
+        y_train = train_sc_df.dropna()[target_names]
+
+        X_test = test_sc_df.dropna().drop(target_names, axis=1)
+        y_test = test_sc_df.dropna()[target_names]
+
+        X_train = X_train.values
+        X_test = X_test.values
+
+        y_train = y_train.values
+        y_test = y_test.values
+
+        X_train_t = X_train.reshape(X_train.shape[0], 177, 1)
+        X_test_t = X_test.reshape(X_test.shape[0], 177, 1)
+
+        ## Number of Data ##
+        self.df_temp = df_temp
+        self.train = train
+        self.test = test
+        #######################
+
+        self.X_train, self.X_test, self.y_train, self.y_test = X_train, X_test, y_train, y_test
+        self.X_train_t = X_train_t
+        self.X_test_t = X_test_t
+
+    def totalDataNumber(self):
+        return str(self.df_temp.values.shape[0])
+
+    def trainDataNumber(self):
+        return str(self.train.shape[0])
+
+    def testDataNumber(self):
+        return str(self.test.values.shape[0])
+
+
+def load_data(fname):
+    df = pd.read_csv(fname, engine='python')
+    df['date'] = pd.to_datetime(df['date']) + Minute(1)
+    df = df.set_index('date')
+    target_names = ['inner_temperature', 'inner_humidity', 'inner_co2']
+    df_targets = df[target_names]
+
+    return df_targets
+
+
+if __name__ == '__main__':
+    main()
+
--- main.py
+++ main.py
@@ -1,2 +1,436 @@
 import sys
-print("test")
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import QDate
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QIcon
+
+import ezKeras
+
+from PIL import Image
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
+
+
+class MyApp(QDialog):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        # Tab
+        tabs = QTabWidget()
+        tabs.addTab(FirstTab(), 'Dataset')
+        tabs.addTab(SecondTab(), 'Learning')
+        tabs.addTab(ThirdTab(), 'Compare Model')
+
+        buttonbox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        buttonbox.accepted.connect(self.accept)
+        buttonbox.rejected.connect(self.reject)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(tabs)
+        vbox.addWidget(buttonbox)
+
+        self.setLayout(vbox)
+
+        self.setWindowTitle('EZinfotech')
+        self.setGeometry(300, 300, 400, 300)
+        self.center()
+        self.show()
+
+    def center(self):
+        self.setWindowTitle('EZinfotech')
+        self.setGeometry(300, 300, 1000, 1000)
+        self.show()
+
+        qr = self.frameGeometry() # 창 위치와 크기 정보 get
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    # Close 시 MsgBox
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message', 'Are you sure to quit?',
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
+##### Dataset & Hyper Parameter Tap #####
+class FirstTab(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        # Dataset file Open
+        self.fname = [] # filename
+        self.total_le = QLineEdit()
+        self.train_le = QLineEdit()
+        self.test_le = QLineEdit()
+        self.dir_le = QLineEdit()
+
+        # Dataset Head Display
+        self.head_te = QTextEdit()
+
+        # Learning Instance
+        self.machine = ezKeras.Machine()
+        #machine.run(epochs=1, units=1, batch_size=512)
+
+        # Dataset Graph
+        self.datasetgraph = QLabel()
+
+        # UI initialize
+        self.initUI()
+
+    def initUI(self):
+        grid = QGridLayout()
+        grid.addWidget(self.createDatasetGroup(), 0, 0)
+        grid.addWidget(self.createHyperParameterGroup(), 0, 1)
+
+        grid.addWidget(self.createTrainingModelGroup(), 1, 0)
+        grid.addWidget(self.createTrainingManageGroup(), 1, 1)
+
+        grid.addWidget(self.createTrainingLoggingGroup(), 2, 0)
+        grid.addWidget(self.createTrainingResultGroup(), 2, 1)
+
+        self.setLayout(grid)
+
+        # Button
+        self.setButton()
+
+    # Button setting
+    def setButton(self):
+        # buttons
+        startBtn = QPushButton('Load', self)
+        startBtn.setToolTip('<b>Training</b>')
+        startBtn.move(850, 800/3)
+        startBtn.resize(startBtn.sizeHint())
+
+        setBtn = QPushButton('Save', self)
+        setBtn.setToolTip('<b>Set Parameter</b>')
+        setBtn.move(850, 800/3 - 25)
+        setBtn.resize(setBtn.sizeHint())
+
+        # Quit
+        #open_btn.clicked.connect(QCoreApplication.instance().quit)
+
+    # 불러온 파일 이름 저장
+    def show_dialog(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', './')
+
+        if fname[0]:
+            #pixmap = QPixmap(fname[0])
+            #self.label.append(QLabel())
+            #self.label[-1].setPixmap(pixmap.scaledToWidth(pixmap.width() * 1.1))
+            self.fname.append(fname[0])
+            print(self.fname)
+            #self.x_le.append("파일 경로 : " + str(self.fname))
+            # self.dir_le.setText(str(self.fname))
+            self.dir_le.setText(fname[0])
+            #machine = ezKeras.Machine(fname[0])
+            self.machine.datasetLoad(fname[0])
+            #self.machine.run(epochs=1, units=1, batch_size=512)
+
+            self.total_le.setText(str(self.machine.totalDatasetNumber()))
+            self.train_le.setText(str(self.machine.trainDatasetNumber()))
+            self.test_le.setText(str(self.machine.testDatasetNumber()))
+
+            self.head_te.setText(str(self.machine.datasethead()))
+
+
+
+            # Dataset 호출
+
+            #self.hbox_label.addWidget(self.label[-1])
+
+    def createDatasetGroup(self):
+
+        groupbox = QGroupBox('■ Dataset Preview')
+
+        layout = QVBoxLayout()
+
+        hbox_total = QHBoxLayout()
+        #hbox_xy.addStretch(5)
+        hbox_total.addWidget(QLabel(' ● Total : '))
+        hbox_total.addWidget(self.total_le)
+        self.total_le.setAlignment(Qt.AlignLeft)
+
+        hbox_train = QHBoxLayout()
+        hbox_train.addWidget(QLabel(' ● Train : '))
+        hbox_train.addWidget(self.train_le)
+        self.train_le.setAlignment(Qt.AlignLeft)
+
+        hbox_test = QHBoxLayout()
+        hbox_test.addWidget(QLabel(' ● Test : '))
+        hbox_test.addWidget(self.test_le)
+        self.test_le.setAlignment(Qt.AlignLeft)
+
+
+        hbox_dir = QHBoxLayout()
+        hbox_dir.addWidget(QLabel(' ● Directory : '))
+        hbox_dir.addWidget(self.dir_le)
+        # layout.addStretch(1)
+
+        hbox_head = QVBoxLayout()
+        hbox_head.addWidget(QLabel(' ● Dataset Sample (50 Indexes) : '))
+        self.head_te = QTextEdit(self)
+        self.head_te.setText("Dataset head()")
+        self.head_te.resize(self.head_te.sizeHint())
+        hbox_head.addWidget(self.head_te)
+
+        hbox_open = QHBoxLayout()
+        open_btn2  = QPushButton('Open Dataset (*.csv)')
+        open_btn2.resize(open_btn2 .sizeHint())
+        open_btn2.clicked.connect(self.show_dialog)
+        hbox_open.addWidget(open_btn2)
+
+        #
+        #hbox_xy.addStretch(3)
+        layout.addLayout(hbox_total)
+        layout.addLayout(hbox_train)
+        layout.addLayout(hbox_test)
+        layout.addLayout(hbox_dir)
+        layout.addLayout(hbox_head)
+        layout.addLayout(hbox_open)
+
+
+        layout.addStretch(1)
+        groupbox.setLayout(layout)
+
+        return groupbox
+
+    # Hyper Parameter
+    def createHyperParameterGroup(self):
+        groupbox = QGroupBox('Set Hyper Parameters')
+        groupbox.setFlat(True)
+
+        lbl1 = QLabel('Epoch')
+        spinbox = QSpinBox()
+        spinbox.setMinimum(-10)
+        spinbox.setMaximum(30)
+        # self.spinbox.setRange(-10, 30)
+        spinbox.setSingleStep(2)
+        #lbl2 = QLabel('0')
+
+        #spinbox.valueChanged.connect(self.value_changed)
+        #lbl2.setText(str(spinbox.value()))
+
+        lbl2 = QLabel('Batch_size')
+        spinbox2 = QSpinBox()
+        spinbox2.setMinimum(-10)
+        spinbox2.setMaximum(30)
+        spinbox2.setSingleStep(2)
+
+        # Activation
+        lbl3 = QLabel('Activation Func')
+        spinbox3 = QSpinBox()
+        spinbox3.setMinimum(-10)
+        spinbox3.setMaximum(30)
+        spinbox3.setSingleStep(2)
+
+        vbox = QVBoxLayout()
+
+        vbox.addWidget(lbl1)
+        vbox.addWidget(spinbox)
+
+        vbox.addWidget(lbl2)
+        vbox.addWidget(spinbox2)
+
+        vbox.addWidget(lbl3)
+        vbox.addWidget(spinbox3)
+
+        vbox.addStretch()
+
+        groupbox.setLayout(vbox)
+
+        return groupbox
+
+    # Dataset head
+    def createTrainingModelGroup(self):
+        groupbox = QGroupBox('Dataset Sample (50 Indexes)')
+        groupbox.setFlat(True)
+        return groupbox
+
+
+    # Training Start, save, load
+    def createTrainingManageGroup(self):
+
+        groupbox = QGroupBox('Dataset Normal Distribution ')
+        #groupbox.setCheckable(True)
+        groupbox.setChecked(True)
+
+        pushbutton = QPushButton('Training Start')
+        togglebutton = QPushButton('Model Load')
+        togglebutton.setCheckable(True)
+        togglebutton.setChecked(True)
+
+        vbox = QVBoxLayout()
+        #vbox.addWidget(pushbutton)
+        #vbox.addWidget(togglebutton)
+        vbox.addStretch(1)
+        groupbox.setLayout(vbox)
+
+        return groupbox
+
+    def createTrainingLoggingGroup(self):
+        # groupbox = QGroupBox('Training Log')
+        # groupbox.setFlat(True)
+
+        groupbox = QGroupBox('Dataset Graph')
+        groupbox.setFlat(True)
+
+        #######################
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.datasetgraph)
+        #im = Image.open('C:\Users\PJ\Desktop\test_dirrush_hour.png')
+        pixmap = QPixmap('C:\\Users\PJ\Desktop\\test_dir\co2.png')
+        self.datasetgraph.setPixmap(pixmap.scaledToWidth(pixmap.width() * 0.5))
+
+        groupbox.setLayout(hbox)
+        return groupbox
+
+    def createTrainingResultGroup(self):
+
+        # 정규분포 화면
+        groupbox = QGroupBox('-')
+        groupbox.setFlat(True)
+
+        textedit = QTextEdit(self)
+        textedit.setText("(Graph)")
+        textedit.resize(textedit.sizeHint())
+        # txt = self.textedit.toPlainText()
+        # 내용글 얻어오기 self.statusbar.showMessage(txt) # 상태바에 출력
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(textedit)
+        # vbox.addWidget(radio2)
+        # vbox.addWidget(radio3)
+        # vbox.addWidget(checkbox)
+        #vbox.addStretch(1)
+        groupbox.setLayout(vbox)
+        #vbox.addStretch(1)
+
+        return groupbox
+
+
+class SecondTab(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        hyper_group = QGroupBox('Hyper Parameters')
+
+        #groupbox = QGroupBox('Dataset Preview')
+
+        label1 = QLabel('Epochs : ', self)
+        label1.setAlignment(Qt.AlignVCenter)
+
+        label2 = QLabel('Batch_size : ', self)
+        label2.setAlignment(Qt.AlignVCenter)
+
+        label3 = QLabel('Units : ', self)
+        label3.setAlignment(Qt.AlignVCenter)
+
+        font1 = label1.font()
+        font1.setPointSize(11)
+
+        # font2 = label2.font()
+        # font2.setFamily('Times New Roman')
+        # font2.setBold(True)
+
+        label1.setFont(font1)
+        label2.setFont(font1)
+        label3.setFont(font1)
+        # label2.setFont(font2)
+
+        layout = QVBoxLayout()
+        layout.addWidget(label1)
+        layout.addWidget(label2)
+        layout.addWidget(label3)
+        layout.addStretch(1)
+
+        hyper_group.setLayout(layout)
+        #groupbox.setLayout(layout)
+        hyper_group.setLayout(layout)
+        #hyper_group.setLayout(vbox1)
+
+        ####################################
+        logging_group = QGroupBox('Training Log')
+
+        vbox2 = QVBoxLayout()
+
+        textedit = QTextEdit(self)
+        textedit.setText("(Epoch)")
+        textedit.resize(textedit.sizeHint())
+        # txt = self.textedit.toPlainText()
+        # 내용글 얻어오기 self.statusbar.showMessage(txt) # 상태바에 출력
+
+        #vbox = QVBoxLayout()
+        vbox2.addWidget(textedit)
+
+
+        logging_group.setLayout(vbox2)
+
+        ##############################
+        result_group = QGroupBox('Result Graph')
+
+        vbox3 = QVBoxLayout()
+
+        textedit2 = QTextEdit(self)
+        textedit2.setText("(Graph)")
+        textedit2.resize(textedit2.sizeHint())
+        # txt = self.textedit.toPlainText()
+        # 내용글 얻어오기 self.statusbar.showMessage(txt) # 상태바에 출력
+
+        # vbox = QVBoxLayout()
+        vbox3.addWidget(textedit2)
+
+        result_group.setLayout(vbox3)
+        #####
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(hyper_group)
+        vbox.addWidget(logging_group)
+        vbox.addWidget(result_group)
+        self.setLayout(vbox)
+
+
+class ThirdTab(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        lbl = QLabel('Terms and Conditions')
+        text_browser = QTextBrowser()
+        text_browser.setText('This is the terms and conditions')
+        checkbox = QCheckBox('Check the terms and conditions.')
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lbl)
+        vbox.addWidget(text_browser)
+        vbox.addWidget(checkbox)
+
+        self.setLayout(vbox)
+
+
+if __name__ == '__main__':
+    main()
